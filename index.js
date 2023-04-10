f76b173c8f005758db715e4f29bb5b32377600fb
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2023-04-10 18:08:52 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const http = require('http');
-
-http.createServer(function(req,res){
-  console.log('createServer got request')
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/': 
-      serveStatic(res, '/../public/home.html', 'text/html');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404:Page not found.');
-  }
-  
+const http = require("http"); 
+http.createServer((req,res) => {
+    var path = req.url.toLowerCase();    
+    switch(path) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Home page');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('About page');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }    
 }).listen(process.env.PORT || 3000);
-console.log('after createServer')
