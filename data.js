84de3124066eb2e3f0820f457277455c196a57f5
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2023-04-12 23:49:18 +0000

    added data.js file and objects

---
+++ data.js
@@ -0,0 +1,5 @@
+let showcase = [
+    {"name": "Din Djarin", "height": "1.8 meters", "homeworld": "Concordia", "clan": "Mandolorian" },
+    {"designation": "USS Enterprise (NCC - 1701 - D", "class": "Galaxy", "armaments": "Phasers, Photon Torpedos", "max speed": "Warp 9.5"},
+    {}
+]
