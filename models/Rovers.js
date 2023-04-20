0d2cd17e098b0b4df31e85c5a3805d9db9b1ec07
nvialp <18348472-nvialp@users.noreply.replit.com>
2023-04-20 04:24:25 +0000

    added models directory and rovers.js file

---
+++ models/Rovers.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+// For security, connectionString should be in a separate file and excluded from git
+const connectionString = "mongodb+srv://Admin:5XMkg6aJyP0AJNr9@cluster0.doumi3o.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+    dbName: 'class-projects',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('open', () => {
+  console.log('Mongoose connected.');
+});
+
+// define data model as JSON key/value pairs
+// values indicate the data type of each key
+const roverSchema = new Schema({
+ name: { type: String, required: true },
+ landed: String,
+ speed: String,
+ mass: String,
+ tools: int
+});
+
+export const Book = mongoose.model('Rover', roverSchema);
