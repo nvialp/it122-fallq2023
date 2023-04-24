3931f221a665b1588ea590ca6e1837992b57eac3
nvialp <114171633+nvialp@users.noreply.github.com>
2023-04-24 01:03:06 +0000

    updated home route to use mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as http from 'http';
 import * as querystring from 'querystring';
 import * as data from './data.js';
+import { Rovers } from "./models/Rovers.js";
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -9,8 +10,17 @@ app.use(express.static('./public'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//defined routes with mongodb
+app.get('/', (req, res, next) => {
+    Rovers.find({}).lean()
+      .then((rovers) => {
+        // respond to browser only after db query completes
+        res.render('home', { rovers });
+      .catch(err => next(err))
+});
+
 //defined routes
-app.get('/', (req,res) => {
+/*app.get('/', (req,res) => {
   res.render('home', { rovers: data.getAll()});
 });
 
@@ -18,7 +28,7 @@ app.get('/rover/:name', (req, res) => {
     const name = req.params.name;
 
     res.render('detail', { rover: data.getItem(name)});
-})
+})*?
 
 // define 404 handler
 app.use((req,res) => {
